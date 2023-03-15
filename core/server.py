26aeed360ed83bdceda4ddc9d2d33d59050f50e0
Nameies <nameies@yandex.ru>
2023-03-15 15:23:48 +0300

    Added docstrings. Removed useless function.

--- core/server.py
+++ core/server.py
@@ -1,11 +1,12 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from core.gamestate import GameState
-from typing import Callable
 
 
 class DotaGSIRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        """Send data to GameState class
+        """
         length = int(self.headers["Content-length"])
         self.server.gamestate.loads(json.loads(self.rfile.read(length)))
 
@@ -15,11 +16,18 @@ class DotaGSIServer(HTTPServer):
         self.gamestate = GameState()
         super().__init__(*args, **kwargs)
 
-    def run(self):
-        self.serve_forever()
 
+def get_server(server_class=DotaGSIServer, handler_class=DotaGSIRequestHandler, server_address=("", 1417)) -> HTTPServer:
+    """Returns server instance with specified parameters.
 
-def get_server(server_class=DotaGSIServer, handler_class=DotaGSIRequestHandler, server_address=("", 1417)):
+    Args:
+        server_class (HTTPServer, optional): HTTP server class. Defaults to DotaGSIServer.
+        handler_class (HTTPRequestHandler, optional): HTTP request handler class. Defaults to DotaGSIRequestHandler.
+        server_address (tuple, optional): IP address and port in tuple. Defaults to ("", 1417).
+
+    Returns:
+        HTTPServer: HTTP server object.
+    """
     httpd = server_class(server_address, handler_class)
 
     return httpd
