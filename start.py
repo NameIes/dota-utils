12fb6f726e461216b0cfc772cb34b6cf62976286
Nameies <nameies@yandex.ru>
2023-03-15 06:52:00 +0300

    Configured dota GSI.

---
+++ start.py
@@ -0,0 +1,14 @@
+from server import get_server
+from gui import GUI
+import threading
+
+
+if __name__ == "__main__":
+    server = get_server()
+    gui = GUI()
+    server.gamestate.on_update(gui.on_update_gamestate)
+    thread = threading.Thread(None, server.run)
+    thread.start()
+    gui.mainloop()
+    server.shutdown()
+    thread.join()
